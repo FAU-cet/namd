e3ba81e525279658a6daa8cbfe297b6e05b9794b
Johannes Dittrich <johannes.dittrich@fau.de>
2024-06-09 21:22:25 +0200

    refactor

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@
 rm -rf namd
 echo "[SCVL] cleaned namd folder"
 
-for folder in benchmarks namd-bin
+for folder in benchmarks namd-bin/precompiled
 do
     find $folder/* ! -name '.gitkeep' -type d -exec rm -rf {} +
     echo "[SCVL] cleaned $folder"
