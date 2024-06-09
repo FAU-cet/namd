432ae00d97b1776e11b3d9a54ee6157de614164b
Johannes Dittrich <johannes.dittrich@fau.de>
2024-06-09 21:50:23 +0200

    cute cat!

---
+++ give_me_everything.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+scripts="download_benchmarks.sh download_binaries.sh download_namd.sh"
+
+run() {
+    ./$1
+
+    echo "[SCVL] ran script $1"
+    touch $1.sync
+}
+
+# launch rockets
+for script in $scripts
+do
+    rm -f $script.sync
+    run $script &
+done
+
+# wait for impact
+for sync in $scripts
+do
+    while [ ! -e $sync.sync ]
+    do
+        sleep 1
+    done
+    rm $sync.sync
+done
+
+echo
+echo "[SCVL] かわいい猫! 🐱🐱🐱"
