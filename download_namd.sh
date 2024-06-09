f499866f09f70bd063a306052390c69430e953d4
Marcel Heckel <scvl127h@alex1.nhr.fau.de>
2024-06-09 19:27:01 +0200

    added a downloading script for namd and all its dependencies

---
+++ download_namd.sh
@@ -0,0 +1,39 @@
+#! /bin/sh
+
+
+#download the latest binarys for namd from the repos
+rm -rf namd
+git clone git@gitlab.com:tcbgUIUC/namd.git
+cd namd
+
+
+#import charm++
+git clone https://github.com/UIUC-PPL/charm
+mv charm charm-v7.0.0
+
+#download the other necessary binaries
+wget http://www.ks.uiuc.edu/Research/namd/libraries/fftw-linux-x86_64.tar.gz
+wget http://www.ks.uiuc.edu/Research/namd/libraries/tcl8.6.13-linux-x86_64.tar.gz
+wget http://www.ks.uiuc.edu/Research/namd/libraries/tcl8.6.13-linux-x86_64-threaded.tar.gz
+
+tar xzf fftw-linux-x86_64.tar.gz
+tar xzf tcl8.6.13-linux-x86_64.tar.gz
+tar xzf tcl8.6.13-linux-x86_64-threaded.tar.gz
+
+rm *.tar.gz
+
+mv linux-x86_64 fftw
+mv tcl8.6.13-linux-x86_64 tcl
+mv tcl8.6.13-linux-x86_64-threaded tcl-threaded
+
+
+#module load cuda
+#./config Linux-x86_64-g++ --charm-arch multicore-linux-x86_64 --with-single-node-cuda --cuda-prefix $CUDA_HOME
+
+#mkdir bin
+
+#cd Linux-x86_64-g++
+
+#make
+
+#mv namd3 ../bin
