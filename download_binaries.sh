51bbffd261b7f961c045b018de53d9a844ade24e
Ashley Heckel <heckel.marcel@gmx.de>
2024-09-08 15:41:50 +0200

    updated links to pull the release versions instead of the beta versions

--- download_binaries.sh
+++ download_binaries.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # Version 3.0b7
-url="https://www.ks.uiuc.edu/Research/namd/3.0b7/download/231945/"
+url="https://www.ks.uiuc.edu/Research/namd/3.0/download/342056/"
 bins="multicore multicore-AVX512 multicore-CUDA netlrts-smp netlrts-smp-CUDA verbs-smp verbs-smp-CUDA"
 
 download_namd() {
-    fullnamd="NAMD_3.0b7_Linux-x86_64-$1"
+    fullnamd="NAMD_3.0_Linux-x86_64-$1"
     tarball=$fullnamd.tar.gz
 
     wget "$url/$tarball"
