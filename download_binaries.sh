ec3e74ea6a005caf04a26b07c294785c2106b514
jojodicus <jojodicus@gmail.com>
2024-10-21 22:39:58 +0200

    version update 3.0.1

--- download_binaries.sh
+++ download_binaries.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 # Version 3.0b7
-url="https://www.ks.uiuc.edu/Research/namd/3.0/download/342056/"
+# url="https://www.ks.uiuc.edu/Research/namd/3.0/download/342056/"
+# Version 3.0.1
+url="https://www.ks.uiuc.edu/Research/namd/3.0.1/download/453167/"
 bins="multicore multicore-AVX512 multicore-CUDA netlrts-smp netlrts-smp-CUDA verbs-smp verbs-smp-CUDA"
 
 download_namd() {
-    fullnamd="NAMD_3.0_Linux-x86_64-$1"
+    # fullnamd="NAMD_3.0_Linux-x86_64-$1"
+    fullnamd="NAMD_3.0.1_Linux-x86_64-$1"
     tarball=$fullnamd.tar.gz
 
     wget "$url/$tarball"
